f3f824ac8c4f1d5a91f21debdfff2f97cb239850
Rubens Altimari <rubens@introspection.com.br>
2020-04-01 16:52:28 +0200

    Creating a Python package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='mybgg',
+    version='0.1',
+    scripts=['mybgg'] ,
+    author="Rubens Altimari",
+    author_email="rubens@altimari.nl",
+    description="A command-line tool to query someone's game collection at boardgamegeek.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dalembertian/mybgg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+     ],
+    python_requires='>=3.6',
+ )
