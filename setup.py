dd5a4f913d18f843d5c5882248e4078b1b4bb8e6
Rubens Altimari <rubens@introspection.com.br>
2023-09-06 00:25:05 +0200

    Bumping version to 1.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mybgg',
-    version='1.5',
+    version='1.6',
     # scripts=['mybgg'] ,
     author="Rubens Altimari",
     author_email="rubens@altimari.nl",
